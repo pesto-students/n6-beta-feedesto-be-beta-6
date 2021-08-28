ad779177922d24e90dd14122848e7d0c23716822
Harshit Sangani <iamharshitsangani@gmail.com>
2021-08-28 18:12:21 +0530

    cleanup: server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
-import { express, NotFoundError, sendResponseError, WebApiServer } from '@hkbyte/webapi'
-import apiRoutes from './controller/apis/routes'
-import configs from './core/configs'
+import { NotFoundError, sendResponseError, WebApiServer } from "@hkbyte/webapi"
+import apiRoutes from "./controller/apis/routes"
+import configs from "./core/configs"
 
 const server = new WebApiServer(configs.server.port, { cors: true, helmet: true })
 
-server.addMiddlewares(express.static('static'))
 server.addWebApis(...apiRoutes)
 
 server.addMiddlewares((_req, res, next) => {
-	sendResponseError(res, new NotFoundError('Request API not found'), {})
+	sendResponseError(res, new NotFoundError("Request API not found"), {})
 	next()
 })
 
