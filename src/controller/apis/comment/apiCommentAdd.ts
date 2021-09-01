b94e81f718166ec1d296bb928c2202e20451d349
Harshit Sangani <iamharshitsangani@gmail.com>
2021-09-01 19:24:41 +0530

    fix: commentAdd

--- src/controller/apis/comment/apiCommentAdd.ts
+++ src/controller/apis/comment/apiCommentAdd.ts
@@ -1,5 +1,6 @@
 import { RequestMethod, T, WebApi } from "@hkbyte/webapi"
 import { addComment } from "../../../services/mongo/comment"
+import { RequestLocals } from "../../../utils/types"
 import { AuthRole } from "../../auth"
 import authMiddleware from "../../middlewares/authMiddleware"
 
@@ -9,18 +10,22 @@ type Context = {
 		userId: string
 		content: string
 	}
+	locals: RequestLocals
 }
 
 export const apiCommentAdd = new WebApi({
-	endpoint: "/answer",
+	endpoint: "/comment",
 	requestBodySchema: T.object({
 		answerId: T.string().mongoObjectId(),
-		userId: T.string().mongoObjectId(),
+		userId: T.string().mongoObjectId().optional(),
 		content: T.string().trim().nonEmpty(),
 	}),
 	method: RequestMethod.POST,
 	middlewares: [authMiddleware(AuthRole.ORGANIZATION, AuthRole.USER)],
-	handler: async ({ body }: Context) => {
+	handler: async ({ body, locals }: Context) => {
+		if (locals.session.role === AuthRole.USER) {
+			body.userId = locals.session.userId
+		}
 		const created = await addComment(body)
 		return { created }
 	},
