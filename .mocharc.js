fb2480a78ffcd9ebf9625bf50b5849fb402cb79b
Harshit Sangani <iamharshitsangani@gmail.com>
2021-09-13 10:57:19 +0530

    update: package.json updated commands for seeding and testing remove: old seeding files

--- .mocharc.js
+++ .mocharc.js
@@ -1,11 +1,11 @@
-const dir = process.argv.length > 2 ? process.argv[2] + '/**' : '**'
+const dir = process.argv.length > 2 ? process.argv[2] + "/**" : "**"
 const spec = `test/${dir}/*.test.ts`
 
 module.exports = {
 	timeout: 20000,
 	exit: true,
-	extension: 'ts',
+	extension: "ts",
 	recursive: true,
-	require: 'ts-node/register',
+	require: "ts-node/register",
 	spec,
 }
