abe1ea300b778da5d45c1def40e2aeeb6371c924
Harshit Sangani <iamharshitsangani@gmail.com>
2021-09-17 04:17:29 +0530

    test: GET /user/get

--- test/apis/user/user.get.api.test.ts
+++ test/apis/user/user.get.api.test.ts
@@ -6,7 +6,7 @@ import { generateOrganization } from "../../resources/organization"
 import { generateUser } from "../../resources/user"
 import { WebApiResponseStatus } from "../../setup"
 
-const endpoint = "/user"
+const endpoint = "/user/get"
 
 describe(`API: ${endpoint}`, () => {
 	let loginUserId: string
@@ -27,31 +27,6 @@ describe(`API: ${endpoint}`, () => {
 		await generateUser({ organizationId: loginOrganizationId })
 	})
 
-	it("Success: all users as admin", async () => {
-		const res = await webApiRequest(endpoint, {
-			method: "get",
-			auth: {
-				organizationId: loginOrganizationId,
-				role: AuthRole.ORGANIZATION,
-				userId: loginUserId,
-			},
-		})
-		expect(res).length(2)
-
-		expect(res)
-			.to.be.an("array")
-			.include.a.thing.with.any.keys(
-				"_id",
-				"name",
-				"organizationId",
-				"organization",
-				"verified",
-				"verifiedAt",
-				"createdAt",
-				"updatedAt",
-			)
-	})
-
 	it("Fail: all users as public", async () => {
 		await webApiRequest(endpoint, {
 			method: "get",
@@ -59,14 +34,30 @@ describe(`API: ${endpoint}`, () => {
 		})
 	})
 
-	it("Fail: all users as user", async () => {
-		await webApiRequest(endpoint, {
-			method: "get",
-			auth: {
-				role: AuthRole.USER,
-				userId: loginUserId,
-			},
-			expectedStatus: WebApiResponseStatus.FORBIDDEN,
+	Object.values(AuthRole).map((role) => {
+		it(`Success: get user as ${role}`, async () => {
+			const res = await webApiRequest(endpoint, {
+				method: "get",
+				auth: {
+					organizationId: loginOrganizationId,
+					role: role,
+					userId: loginUserId,
+				},
+			})
+
+			expect(res)
+				.to.be.an("array")
+				.of.length(1)
+				.include.a.thing.with.any.keys(
+					"_id",
+					"name",
+					"organizationId",
+					"organization",
+					"verified",
+					"verifiedAt",
+					"createdAt",
+					"updatedAt",
+				)
 		})
 	})
 })
